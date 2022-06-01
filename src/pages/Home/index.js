d6405bc9d2d56418b8d75346d1eb0797c14bc8ac
Lucas <lviana1550@gmail.com>
2022-06-01 02:13:19 -0300

    interface e utilização api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,94 @@
-import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, FlatList } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons'
+import api from '../../services/api'
+import { getFavorite,setFavorite } from '../../services/favorite';
+
+import CategoryItem from '../../components/CategoryItem';
+import { FavoritePost } from '../../components/FavoritePost';
 
 export default function Home(){
-
    const navigation = useNavigation();
+   const [categories, setCategories] = useState([]);
+   const [favCategory, setFavCategory] = useState([]);   
 
+   useEffect(() => {
+      
+      async function loadData() {
+         const category = await api.get("/api/categories?populate=icon")
+         setCategories(category.data.data)
+      }
 
+      loadData();
+   
+   }, [])
+
+   useEffect(() => {
+      async function favorite(){
+         const response = await getFavorite()
+         setFavCategory(response);
+      }
+
+      favorite();
+   }, [])
+
+   //Favoritando uma categoria
+
+   async function handleFavorite(id){
+      const response = await setFavorite(id)
+
+      setFavCategory(response);
+      //alert("Categ Favoritada")
+   }
+   
    return(
       <SafeAreaView style={styles.container}>
          <View style={styles.header}>
             <Text style={styles.name}>DevBlog</Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={ () => navigation.navigate("Search") }>
                <Feather name='search' size={24} color='#FFF'/>
             </TouchableOpacity>
+         </View>
+
+         <FlatList
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            contentContainerStyle={{ paddingRight: 12 }} 
+            style={styles.categories}
+            data={categories}
+            keyExtractor={ (item) => String(item.id) }
+            renderItem={ ({item}) => (
+            <CategoryItem
+               data={item}
+               favorite={ () => handleFavorite(item.id) }
+            />
+            ) }
+         />
+
+         <View style={styles.main}>
+            
+            {favCategory.length !== 0 && (
+               <FlatList
+                  style={{marginTop: 50, maxHeight: 100, paddingStart: 18,}}
+                  contentContainerStyle={{paddingEnd: 18}}
+                  data={favCategory}
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                  keyExtractor={ (item) => String(item.id) }
+                  renderItem={ ({ item }) => <FavoritePost data={item} />}
+               />
+            )}
+
+            <Text 
+            style={[
+               styles.title,
+               { marginTop: favCategory.length > 0 ? 14 : 46 }
+            ]}>
+               Conteúdo em alta
+            </Text>
 
          </View>
       </SafeAreaView>
@@ -40,5 +112,24 @@ const styles = StyleSheet.create({
       fontSize: 28,
       color: '#FFF',
       fontWeight: 'bold'
+   },
+   categories:{
+      maxHeight: 115,
+      backgroundColor: '#EFEFEF',
+      marginHorizontal: 18,
+      borderRadius: 8,
+      zIndex: 9
+   },
+   main:{
+      backgroundColor: '#FFF',
+      flex: 1,
+      marginTop: -30,
+   },
+   title:{
+      fontSize: 21,
+      paddingHorizontal: 18,
+      marginBottom: 14,
+      fontWeight: 'bold',
+      color: '#162133'
    }
 })
